6cf4bc82cb3ec307134a0792cab6c0cf394cdcdc
abbashassan-py <abbashassan.rh@gmail.com>
2022-10-10 18:10:34 +0500

    added dateNtime to email

--- Testing-task/Data/Data.cs
+++ Testing-task/Data/Data.cs
@@ -10,7 +10,7 @@ namespace Testing_task.Data
     {
         public static object[] TestDataForAccount =
         {
-            new object [] {"HaiderHassan11032000@conntour-sofware.com","Mr.", "abbas", "hassan", "Password1", 7, "July", 2022, "Sign up for our newsletter!",
+            new object [] {$"HaiderHassan{ DateTime.Now.ToString("MM/dd/yyy hh:mm tt").Replace(" ","").Replace("/","").Replace(":","")}@conntour-sofware.com","Mr.", "abbas", "hassan", "Password1", 7, "July", 2022, "Sign up for our newsletter!",
                             "Company", "Address1", "Address2", "City1", "Alabama", 35004, "Additional Info", 452778, 911345329, "Allias 1" }
 
         };
